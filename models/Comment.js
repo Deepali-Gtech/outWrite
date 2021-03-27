2c9bf94fee19c8b0155622879533ae3a1f88c8b8
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-27 13:32:17 -0400

    corrected model and module name

---
+++ models/Comment.js
@@ -0,0 +1,56 @@
+// importing 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+// creating Comment model
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+    
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        },
+        parent_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        prompt_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+     
+    }, 
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+   
+    
+);
+
+module.exports = Comment;

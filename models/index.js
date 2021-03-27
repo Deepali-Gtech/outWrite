35eda5f0529de92a25aab56d56301a62a8a409bf
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-27 14:01:21 -0400

    created associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,42 @@
+// import models
 const User = require('./User');
+const Category = require('./Category');
+const Comment = require('./Comment');
+const Prompt = require('./Prompt');
+// const Tag = require('./Tag');
 
-module.exports = { User };
+// Prompt belongsTo User
+Prompt.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Prompt belongsTo Category
+Prompt.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+// Prompts have many comments
+Prompt.hasMany(Comment, {
+    foreignKey: 'prompt_id'
+});
+
+// categories have many Prompts
+Category.hasMany(Prompt, {
+    foreignKey: 'category_id'
+});
+
+// Users have many Comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// Users have many Prompts
+User.hasMany(Prompt, {
+    foreignKey: 'user_id'
+});
+
+
+
+
+
+module.exports = { User, Category, Comment, Prompt, };

9b91a35455ca1bece3974d7ffcd240ace8ce4d61
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-29 15:11:00 -0400

    fixed model name in category.js file

--- models/Category.js
+++ models/Category.js
@@ -2,29 +2,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Category extends Model {}
+class Category extends Model { }
 
 // creating Category model
- Category.init(
-     {
-         id: {
-             type: DataTypes.INTEGER,
-             allowNull:false,
-             primaryKey: true,
-             autoIncrement: true,
-         },
-         name: {
-             type: DataTypes.STRING,
-             allowNull: false,
-         },
-         sequelize,
-         timestamps: false,
-         freezeTableName: true,
-         underscored: true,
-         modelName: 'prompt', 
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
 
-     }
+    }
 
- );
+);
 
- module.exports = Category;
+module.exports = Category;
